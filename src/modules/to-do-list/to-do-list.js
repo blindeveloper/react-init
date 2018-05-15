5ae68657d3194da95bf045bd26ff9b3afdae4af6
Shavkunov, Mykhail <Mykhail.Shavkunov.ctr@sabre.com>
2018-05-15 12:25:55 +0200

    dev

--- src/modules/to-do-list/to-do-list.js
+++ src/modules/to-do-list/to-do-list.js
@@ -3,20 +3,17 @@ import ReactDOM from 'react-dom'
 import {createStore, combineReducers} from 'redux'
 import ToDoReducers from './to-do-list-reducers'
 
+let toDoListRedusor = ToDoReducers.toDoList
+
 class ToDoList extends React.Component {
   constructor(){
     super()
-    this.toDoListRedusor = ToDoReducers.toDoList
-    this.store = createStore(ToDoReducers.toDoList)
-    this.updateState = () => new Object({toDoList: this.store.getState()})
-    // this.updateState = () => combineReducers({
-    //   this.toDoListRedusor
-    // })
-    this.state = this.updateState()
+    this.store = createStore(combineReducers({toDoListRedusor}))
+    this.state = this.store.getState()
     
     this.addNewToDoItem = () => {
       this.store.dispatch({type: 'ADD_TO_DO_ITEM', text: this.input.value})
-      this.setState(this.updateState())
+      this.setState(combineReducers({toDoListRedusor}))
       this.input.value = ''
       console.log('store.getState(): ', this.store.getState());
     }
@@ -28,7 +25,7 @@ class ToDoList extends React.Component {
       <input type='text' ref={node => {this.input = node}}/>
       <button onClick={this.addNewToDoItem}>+</button>
      <ul>
-       {this.store.getState().map(e =>
+       {this.store.getState().toDoListRedusor.map(e =>
        <li key={e.id} >
          {e.text}
        </li>)}

94ab8ffc532d9f486cfa63ede612aa5dea352d2e
Shavkunov, Mykhail <Mykhail.Shavkunov.ctr@sabre.com>
2018-05-24 18:37:23 +0200

    strorybook config edit

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,8 +1,8 @@
-import { configure } from '@storybook/react';
+import { configure } from '@storybook/react'
+const req = require.context('../src/components', true, /.stories.js$/)
 
 function loadStories() {
-  require('../src/components/button/index.stories.js');
-  // You can require as many stories as you need.
+  req.keys().forEach(filename => req(filename))
 }
 
-configure(loadStories, module);
+configure(loadStories, module)

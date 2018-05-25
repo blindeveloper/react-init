46ff5cd5ab2f8edbe026ece851da18c8cf745074
Shavkunov, Mykhail <Mykhail.Shavkunov.ctr@sabre.com>
2018-05-25 16:24:39 +0200

    cleanup

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { render } from 'react-dom'
 import { createStore } from 'redux'
-import Root from './components/Root.jsx'
 import rootReducers from './components/organizms/todo/reducers'
-
-import '../src/main.scss'
+import { Provider } from 'react-redux'
+import { BrowserRouter as Router } from 'react-router-dom'
+import App from './components/organizms/App.jsx'
 
 const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
 render(
-  <Root store={store} />,
+  <Provider store={store}>
+    <Router>
+      <App/>
+    </Router>
+  </Provider>,
   document.getElementById('root')
 )
